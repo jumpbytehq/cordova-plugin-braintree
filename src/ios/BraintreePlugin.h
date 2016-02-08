2c9db5a6a1457c1963e324fb8287234cbfd1278a
swapnil7434 <tandel.swapnil@gmail.com>
2016-02-08 12:42:59 +0530

    Update BraintreePlugin.h

--- src/ios/BraintreePlugin.h
+++ src/ios/BraintreePlugin.h
@@ -6,7 +6,7 @@
 
 #import <Foundation/Foundation.h>
 #import <Cordova/CDV.h>
-#import "MBProgressHUD.h"
+//#import "MBProgressHUD.h"
 
 @interface BraintreePlugin : CDVPlugin
 - (void)initialize:(CDVInvokedUrlCommand *)command;
